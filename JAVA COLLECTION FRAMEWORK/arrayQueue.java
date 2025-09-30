9683fa5129d0c302ceb178fa277531de92f44e39
Mo ismail Azeem <141470288+IsmailGaur@users.noreply.github.com>
2025-09-30 23:14:48 +0530

    Add files via upload

---
+++ JAVA COLLECTION FRAMEWORK/arrayQueue.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class arrayQueue {
+    public static void main(String[] args) {
+        Queue<Integer> que = new ArrayDeque<>();
+        que.add(21);
+        que.add(32);
+        que.add(43);
+        que.add(1);
+        que.add(654);
+        que.add(76);
+        System.out.println(que);
+        System.out.println("size of the que Is : " + que.size());
+    }
+
+}
